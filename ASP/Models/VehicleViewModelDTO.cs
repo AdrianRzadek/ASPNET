c3c76d9bc0e1baa28e44cdea320d13a41b12daef
AdrianRzadek <mschxd@gmail.com>
2023-04-07 17:47:49 +0200

    DTO back

---
+++ ASP/Models/VehicleViewModelDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP.Models
+{
+    public class VehicleViewModelDTO
+    {
+        [Required]
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+      // [ForeignKey("VehicleDetailViewModel")]
+     //  public virtual int VehicleId { get; set; }
+
+      //  public virtual VehicleDetailViewModel VehicleDetailViewModel { get; set; }
+
+    }
+}
